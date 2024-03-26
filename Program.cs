29b378d5f960abfc46f8526468b01489021a7bd7
ThomasDC <1496708+thomasdc@users.noreply.github.com>
2024-03-26 22:05:45 +0100

    impl

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
 ﻿// https://codingdojo.org/kata/GameOfLife/
 
-Console.WriteLine(Generation.Parse(File.ReadAllText("input.txt")));
+var generation = Generation.Parse(File.ReadAllText("input.txt"));
+
+do
+{
+    Console.WriteLine(generation);
+    Console.ReadLine();
+    generation = generation.Iterate();
+} while (true);
 
 public record Generation(
     int Iteration, 
@@ -23,4 +30,58 @@ public record Generation(
         ).ToDictionary();
         return new Generation(iteration, width, height, grid);
     }
+
+    public Generation Iterate()
+    {
+        var nextGrid = (
+            from y in Enumerable.Range(0, Height)
+            from x in Enumerable.Range(0, Width)
+            let liveNeighbourCount = (x, y).Neighbours().Count(n => Grid.GetValueOrDefault((n.X, n.Y)))
+            let alive = Grid.GetValueOrDefault((x, y))
+            let nextState = (alive, liveNeighbourCount) switch
+            {
+                (true, < 2) => false,   // 1. Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+                (true, > 3) => false,   // 2. Any live cell with more than three live neighbours dies, as if by overcrowding.
+                (true, 2 or 3) => true, // 3. Any live cell with two or three live neighbours lives on to the next generation.
+                (false, 3) => true,     // 4. Any dead cell with exactly three live neighbours becomes a live cell.
+                (_, _) => alive 
+            }
+            select ((x, y), nextState)
+        ).ToDictionary();
+        return this with { Iteration = Iteration + 1, Grid = nextGrid };
+    }
+
+    public override string ToString()
+    {
+        var builder = new StringBuilder();
+        builder.AppendLine($"Generation {Iteration}:");
+        builder.AppendLine($"{Height} {Width}");
+        for (var y = 0; y < Height; y++)
+        {
+            var innerBuilder = new StringBuilder();
+            for (var x = 0; x < Width; x++)
+            {
+                innerBuilder.Append(Grid.GetValueOrDefault((x, y)) ? '*' : '.');
+            }
+
+            builder.AppendLine(innerBuilder.ToString());
+        }
+        
+        return builder.ToString().TrimEnd();
+    }
+}
+
+public static class Util
+{
+    private static readonly (int xDiff, int yDiff)[] NeighbourVectors = (
+        from x in new[] { -1, 0, 1 }
+        from y in new[] { -1, 0, 1 }
+        where (x, y) is not (0, 0)
+        select (x, y)
+    ).ToArray();
+
+    public static (int X, int Y)[] Neighbours(this (int X, int Y) cell) => (
+            from diff in NeighbourVectors
+            select (cell.X + diff.xDiff, cell.Y + diff.yDiff))
+        .ToArray();
 }
--- Tests.cs
+++ Tests.cs
@@ -29,4 +29,68 @@ public class Tests
             .And.AllSatisfy(x => x.Value.Should().BeTrue())
             .And.ContainKeys((4, 1), (3, 2), (4, 2));
     }
+    
+    [Fact]
+    public void ValidateIteration()
+    {
+        // Arrange
+        const string input = """
+                             Generation 1:
+                             4 8
+                             ........
+                             ....*...
+                             ...**...
+                             ........
+                             """;
+        
+        var generation = Generation.Parse(input);
+
+        // Act
+        var nextGeneration = generation.Iterate();
+
+        // Assert
+        nextGeneration.ToString().Should().Be("""
+                                              Generation 2:
+                                              4 8
+                                              ........
+                                              ...**...
+                                              ...**...
+                                              ........
+                                              """);
+    }
+    
+    [Fact]
+    public Task ValidateIterations()
+    {
+        // Arrange
+        const string input = """
+                             Generation 1:
+                             9 11
+                             .*.........
+                             ..*........
+                             ***........
+                             ...........
+                             ...........
+                             ...........
+                             ...........
+                             ...........
+                             ...........
+                             """;
+        
+        var generation = Generation.Parse(input);
+
+        // Act
+        var generations = new StringBuilder();
+        generations.AppendLine(generation.ToString());
+        generations.AppendLine();
+        foreach (var _ in Enumerable.Range(0, 30))
+        {
+            generation = generation.Iterate();
+            generations.AppendLine(generation.ToString());
+            generations.AppendLine();
+        }
+        
+        // Assert
+        return Verify(generations.ToString());
+    }
 }
