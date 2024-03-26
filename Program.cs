ed9e3fb0c96f3c1bd7898ae80c7058cbfadc34f4
ThomasDC <1496708+thomasdc@users.noreply.github.com>
2024-03-26 20:56:45 +0100

    parse logic

--- Program.cs
+++ Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// https://codingdojo.org/kata/GameOfLife/
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine(Generation.Parse(File.ReadAllText("input.txt")));
+
+public record Generation(
+    int Iteration, 
+    int Width, 
+    int Height, 
+    IReadOnlyDictionary<(int X, int Y), bool> Grid)
+{
+    public static Generation Parse(string input)
+    {
+        var lines = input.Split("\n");
+        var iteration = int.Parse(Regex.Match(lines[0], @"\s(?<iter>\d+):").Groups["iter"].ValueSpan);
+        var dimension = Regex.Match(lines[1], @"(?<height>\d+)\s(?<width>\d+)");
+        var height = int.Parse(dimension.Groups["height"].ValueSpan);
+        var width = int.Parse(dimension.Groups["width"].ValueSpan);
+        var grid = (
+            from y in Enumerable.Range(0, height)
+            from x in Enumerable.Range(0, width)
+            where lines[y + 2][x] is '*'
+            select ((x, y), true)
+        ).ToDictionary();
+        return new Generation(iteration, width, height, grid);
+    }
+}
--- Tests.cs
+++ Tests.cs
@@ -1,9 +1,32 @@
-﻿using Xunit;
+﻿using FluentAssertions;
+using Xunit;
 
 namespace game_of_life;
 
 public class Tests
 {
     [Fact]
-    public void Validate() => Assert.Equal(2, 1 + 1);
+    public void ValidateParsing()
+    {
+        // Arrange
+        const string input = """
+                             Generation 1:
+                             4 8
+                             ........
+                             ....*...
+                             ...**...
+                             ........
+                             """;
+
+        // Act
+        var generation = Generation.Parse(input);
+
+        // Assert
+        generation.Iteration.Should().Be(1);
+        generation.Width.Should().Be(8);
+        generation.Height.Should().Be(4);
+        generation.Grid.Should().HaveCount(3)
+            .And.AllSatisfy(x => x.Value.Should().BeTrue())
+            .And.ContainKeys((4, 1), (3, 2), (4, 2));
+    }
 }
